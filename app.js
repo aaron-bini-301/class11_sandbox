814568dfaee5454304d11bdfa31b46016f654dcc
Aaron Bini <bini.aaron@gmail.com>
2016-06-18 18:02:46 -0700

    moved javascript to separate file

---
+++ app.js
@@ -0,0 +1,25 @@
+// the "notfound" implements a catch-all
+// with page('*', notfound). Here we have
+// no catch-all, so page.js will redirect
+// to the location of paths which do not
+// match any of the following routes
+//
+page.base('/basic');
+
+page('/', index);
+page('/about', about);
+page('/contact', contact);
+page('/contact/:contactName', contact);
+page();
+
+function index() {
+  $('p').text('viewing index');
+}
+
+function about() {
+  $('p').text('viewing about');
+}
+
+function contact(ctx) {
+  $('p').text('viewing contact ' + (ctx.params.contactName || ''));
+}
